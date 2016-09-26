06a1530888413d6c50e9d2bd75d73c992632b889
joe <joeida@jpl.nasa.gov>
2016-09-26 14:43:35 -0700

    reindenting files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,80 +1,44 @@
 exports.sampleUsers = [
-  {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     4,
-     5,
-     1,
-     2,
-     5,
-     4,
-     1
-   ],
-   "compScore": 0
-},
-{
-  "name":"Palm",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-     5,
-     1,
-     4,
-     1,
-     5,
-     1,
-     2,
-     3,
-     2,
-     1
-   ],
-   "compScore": 0
-}
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ],
+        "compScore": 0
+    },
+    {
+        "name":"Palm",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+        5,
+        1,
+        4,
+        1,
+        5,
+        1,
+        2,
+        3,
+        2,
+        1
+        ],
+        "compScore": 0
+    }
 ];
 
 exports.compare = function (newUser) {
-  //calculate sum
-  var self = this;
-  var userSum = this.findSum(newUser);
-  
-  this.sampleUsers[this.sampleUsers.length -1].total = userSum;
+    console.log(exports.sampleUsers);
+    // for (var i = 0; i < exports.sampleUsers.length; i++) {
 
-  //compare to each of the other users
-  var currentBest = {
-    index: 0,
-    diff: 0
-  };
-
-  this.sampleUsers.forEach(function(el){
-    
-    var diff = Math.abs(userSum - self.findSum(el));
-
-    switch (diff) {
-      case ( diff < currentBest.diff):
-        console.log("Found a better match");
-        currentBest.index = self.sampleUsers.indexOf(el);
-        currentBest.diff = diff;
-        break;
-
-      default:
-        currentBest.index = self.sampleUsers.indexOf(el);
-        currentBest.diff = diff;
-    }
-
-    
-  });
-  // console.log("The best match for " + newUser.name  + " is " + this.sampleUsers[currentBest.index].name);
-  this.sampleUsers.push(newUser);
-  return this.sampleUsers[currentBest.index];
-}
-
-exports.findSum = function (u){
-  var sum = 0;
-  u.scores.forEach(function(i){
-    sum += i;
-  });
-  return sum;
-}
+    // }
+    return newUser;
+  }
